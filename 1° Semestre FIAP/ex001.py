9f9376b35546ae7f5df87f2dacc70f3e8b229efc
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-04-25 19:23:59 -0300

    Deu ruim, deixei até o print kkkk

--- 1° Semestre FIAP/ex001.py
+++ 1° Semestre FIAP/ex001.py
@@ -8,13 +8,5 @@ c = erros = 0
 print(f'A palavra tem {quant_char_jogo} letras')
 
 while c != quant_char_jogo:
-    print(hide[0+c], end='')
+    print(hide[c], end='')
     c += 1
-
-while hide != jogo:
-    c = 0
-    chute = input('\nChute uma letra: ')
-    while c == quant_char_jogo:
-        hide[0+c] = chute
-        print(hide)
-        c += 1
