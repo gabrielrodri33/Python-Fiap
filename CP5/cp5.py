29e2e60dc30d3ac1daf39d6297fff086f9ee8727
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-10-07 00:03:56 -0300

    código finalizado

--- CP5/cp5.py
+++ CP5/cp5.py
@@ -1,5 +1,9 @@
 import time
-import random
+import os
+import criar_listas
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 def separador(n, cor):
     cores = {
@@ -25,6 +29,20 @@ def separador(n, cor):
     else:
         mensagem = print(f'{cores[4]["amarelo"]}-={cores[4]["limpa"]}' * n)
     return mensagem
+
+def bubble_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+
+def selection_sort(seq):
+    for i in range(len(seq)):
+        min_index = i
+        for j in range(i+1, len(seq)):
+            if seq[j] < seq[min_index]:
+                seq[j], seq[min_index] = seq[min_index], seq[j]
 
 def insertion_sort(lista):
     for i in range (1, len(lista)):
@@ -66,12 +84,20 @@ def merge_sort(lista):
             j += 1
             k += 1
 
-def selection_sort(seq):
-    for i in range(len(seq)):
-        min_index = i
-        for j in range(i+1, len(seq)):
-            if seq[j] < seq[min_index]:
-                seq[j], seq[min_index] = seq[min_index], seq[j]
+def validacao(var, tipo):
+    if tipo == 1:
+        while var not in ["1","2","3","4","5"]:
+            clear_console()
+            separador(21,5)
+            var = input("Opção inválida\nEscolha o tamanho da lista\n1- 10 mil\n2- 100 mil\n3- 500 mil\n4- 1 milhão\n5- 5 milhões\n").strip()
+        return var
+    
+    elif tipo == 2:
+        while var not in ["1","2","3","4"]:
+            clear_console()
+            separador(21,5)
+            var = input("Opção inválida\nEscolha o método que deseja utilizar\n1- Bubble Sort\n2- Selection Sort\n3- Insertion Sort\n4- Merge Sort\n").strip()
+        return var
 
 def get_time(arg):    
     inicio = time.time()    
@@ -86,14 +112,72 @@ def medir_tempo(limite):
     fim = time.time()
     print(f'Tempo: {fim-inicio}')
 
-def menuPrincipal():
+def carregarListas():
+    return criar_listas.carregarLista("listas.json")
 
+def menuPrincipal():
+    listas = carregarListas()
+    separador(21, 1)
+    tamLista = input("Escolha o tamanho da lista\n1- 10 mil\n2- 100 mil\n3- 500 mil\n4- 1 milhão\n5- 5 milhões\n").strip()
+    tamLista = validacao(tamLista, 1)
+
+    if tamLista == "1":
+        listaDesordenada = listas[0]
+
+    elif tamLista == "2":
+        listaDesordenada = listas[1]
+
+    elif tamLista == "3":
+        listaDesordenada = listas[2]
+
+    elif tamLista == "4":
+        listaDesordenada = listas[3]
+
+    elif tamLista == "5":
+        listaDesordenada = listas[4]
+    
+    clear_console()
+    separador(21, 2)
+
+    metodo = input("Escolha o método que deseja utilizar\n1- Bubble Sort\n2- Selection Sort\n3- Insertion Sort\n4- Merge Sort\n").strip()
+    metodo = validacao(metodo, 2)
+
+    print('Iniciando ordenação')
+    inicio = time.time()
+
+    if metodo == "1":
+        listaOrdenada = bubble_sort(listaDesordenada)
+        fim = time.time()
+
+    elif metodo == "2":
+        listaOrdenada = selection_sort(listaDesordenada)
+        fim = time.time()
+    
+    elif metodo == "3":
+        listaOrdenada = insertion_sort(listaDesordenada)
+        fim = time.time()
+    
+    elif metodo == "4":
+        listaOrdenada = merge_sort(listaDesordenada)
+        fim = time.time()
+        
+    print(f'Tempo de ordenação: {fim-inicio:.3f} segundos')
+
+    
+
+def principal():
+    clear_console()
+    menuPrincipal()
 
 #Programa principal
-lista = [1,2,3,4,5,6,1,3,5,6,2,4,56,1,1,3,8,9,53,4,5,7,2,1]
+principal()
 
-merge_sort(lista)
-get_time(1)
+
+
+# lista = [1,2,3,4,5,6,1,3,5,6,2,4,56,1,1,3,8,9,53,4,5,7,2,1]
+
+# merge_sort(lista)
+# get_time(1)
 # print(lista)
 
 # print(get_time(1000000))
