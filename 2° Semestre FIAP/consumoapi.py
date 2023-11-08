86bf7a0a22fdeecb902b8f3137ca77f0f0565ce8
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-11-08 00:05:59 -0300

    finalizado o código da aula

--- 2° Semestre FIAP/consumoapi.py
+++ 2° Semestre FIAP/consumoapi.py
@@ -1,9 +1,14 @@
 import requests
 
-cep = "87.910-000"
+cep = "02346-000"
 cep = cep.replace("-", "").replace(".", "").strip()
 print(f"CEP: {cep}")
 
 if len(cep) == 8:
     url = f'https://viacep.com.br/ws/{cep}/json'
     
+    requisicao = requests.get(url)
+    print(requisicao)
+
+    dic = requisicao.json()
+    print(dic)
