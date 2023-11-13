d3c52b1622ba0ca8f3f8f1fa486a7d42fa74b78d
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-11-12 23:52:23 -0300

    teste.py

--- Sprint4/teste.py
+++ Sprint4/teste.py
@@ -1,10 +1,32 @@
-import datetime
+import db
 
-def cadastrar_usuario(info, info2):
-    date = datetime.datetime.now()
-    texto = f'-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n{info}: {info2}\nDia e hora: {date}\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n'
-    with open('Sprint4/archive/eventos.txt', 'a') as file:
-        file.write(texto)
+def cadastro_bike():
+    nome = input("Modelo: ")
+    valor_aprox = formata_valor()
+    print(valor_aprox)
+    num_serie = input("Número de série: ")
+    cor = input("Cor: ")
+    obs = input("Observação: ")
+    db.insert_bike_modelo(nome, valor_aprox)
 
+def formata_valor():
+    status = False
+    while not status:
+        try:
+            valor = int(input("Valor: R$"))
+            break
+        except:
+            print("Insira um número!")
 
-cadastrar_usuario('Usuário cadastrado', 'email')
+    if isinstance(valor, (int, float)):
+        valor = round(valor, 2)
+        valor_str = '{:,}'.format(int(valor))
+        if valor == int(valor):
+            valor_str += '.00'
+        else:
+            valor_str += '.' + '{:02d}'.format(int(valor % 1 * 100))
+        return 'R$' + valor_str
+    else:
+        raise ValueError('Invalid valor format')
+    
+cadastro_bike()
