d97c2c03913d30f37ff748a34ddcdd2f70eb8ae4
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-11-13 21:21:15 -0300

    Arquivo de teste.py

--- Sprint4/teste.py
+++ Sprint4/teste.py
@@ -1,32 +1,3 @@
-import db
+import random
 
-def cadastro_bike():
-    nome = input("Modelo: ")
-    valor_aprox = formata_valor()
-    print(valor_aprox)
-    num_serie = input("Número de série: ")
-    cor = input("Cor: ")
-    obs = input("Observação: ")
-    db.insert_bike_modelo(nome, valor_aprox)
-
-def formata_valor():
-    status = False
-    while not status:
-        try:
-            valor = int(input("Valor: R$"))
-            break
-        except:
-            print("Insira um número!")
-
-    if isinstance(valor, (int, float)):
-        valor = round(valor, 2)
-        valor_str = '{:,}'.format(int(valor))
-        if valor == int(valor):
-            valor_str += '.00'
-        else:
-            valor_str += '.' + '{:02d}'.format(int(valor % 1 * 100))
-        return 'R$' + valor_str
-    else:
-        raise ValueError('Invalid valor format')
-    
-cadastro_bike()
+print(random.randint(1, 2))
